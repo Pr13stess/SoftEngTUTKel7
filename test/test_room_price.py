198bc9c63464d1e77b1c7eb6771186c86eb130ed
Roger Ramopolii <ssora6007@gmail.com>
2026-05-25 21:18:22 +0700

    Update test_room_price.py
    adding test case rule

--- test/test_room_price.py
+++ test/test_room_price.py
@@ -20,3 +20,11 @@ def test_invalid_age():
         booking_day="weekday",
         stay_duration=3
         )
+
+def test_guest_adult_family():
+    assert compute_room_price(
+        guest_age=32,
+        room_type="family",
+        booking_day="weekend",
+        stay_duration=2
+    ) == 3600000
